ac00b784d19a13ebce34db4c83cd1caccce81bbd
Brian Breedlove <b.breedlove@outlook.com>
2020-04-15 18:00:49 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot (phrase= "Squawk!", "Pretty Bird")
-  return "#{phrase}, Pretty Bird"
+  puts "#{phrase}, Pretty Bird"
 end
